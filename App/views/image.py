f594b90452817cddd51d495a26202194de52241d
amandalalman <alalman@yahoo.com>
2022-11-26 00:23:53 +0000

    login for image page

--- App/views/image.py
+++ App/views/image.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, jsonify, request, send_from_directory
 from flask_jwt import jwt_required
+from flask_login import login_required, current_user
 
 
 from App.controllers import (
@@ -17,11 +18,13 @@ image_views = Blueprint('image_views', __name__, template_folder='../templates')
 
 
 @image_views.route('/getAllImages', methods=['GET'])
+@login_required
 def get_image_page():
     images = get_all_images()
     return render_template('images.html', images=images)
 
 @image_views.route('/createImage/<userID>', methods=['POST'])
+@login_required
 def create_image_action(userID):
     data = request.form
     user = get_user(userID)
@@ -31,21 +34,25 @@ def create_image_action(userID):
     return jsonify({"message":"User does not exist"}) 
 
 @image_views.route('/api/images', methods=['GET'])
+@login_required
 def get_images_all_action():
     images = get_all_images_json()
     return jsonify(images)
 
 @image_views.route('/api/images/<userID>', methods=['GET'])
+@login_required
 def get_images_by_user_action(userID):
     images = get_images_by_userid_json(userID)
     return jsonify(images)
 
 @image_views.route('/api/images/<imageID>', methods=['GET'])
+@login_required
 def get_images_by_id_action(imageID):
     image = get_image_json(imageID)
     return jsonify(image)
 
 @image_views.route('/api/images/<imageID>', methods=['DELETE'])
+@login_required
 def delete_image_action(imageID):
     if get_image(imageID):
         delete_image(imageID)
